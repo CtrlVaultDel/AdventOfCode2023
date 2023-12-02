230280628ddd67564d3555da50f4281b1982110f
William McGiffert <williammcgiffert@williams-mbp.lan>
2023-12-02 12:49:52 -0600

    AoC Day 2.5

--- Days/2/Wmcgiff/Part1/Program.cs
+++ Days/2/Wmcgiff/Part1/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "./Input/Input.txt";
+        var gamesData = ReadGamesFromFile(filePath);
+
+        int sumOfValidGameIds = CalculateSumOfValidGames(gamesData, 12, 13, 14);
+        Console.WriteLine($"Sum of valid game IDs: {sumOfValidGameIds}");
+    }
+
+    private static List<string> ReadGamesFromFile(string filePath)
+    {
+        var gamesData = new List<string>();
+        try
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    gamesData.Add(line);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error reading the file: {e.Message}");
+        }
+        return gamesData;
+    }
+
+    private static int CalculateSumOfValidGames(List<string> gamesData, int redCubes, int greenCubes, int blueCubes)
+    {
+        int sum = 0;
+        foreach (var game in gamesData)
+        {
+            if (IsGameValid(game, redCubes, greenCubes, blueCubes))
+            {
+                var gameId = int.Parse(Regex.Match(game, @"\d+").Value);
+                sum += gameId;
+            }
+        }
+        return sum;
+    }
+
+    private static bool IsGameValid(string gameData, int redCubes, int greenCubes, int blueCubes)
+    {
+        var sets = gameData.Split(';');
+        foreach (var set in sets)
+        {
+            int red = CountCubes(set, "red");
+            int green = CountCubes(set, "green");
+            int blue = CountCubes(set, "blue");
+
+            if (red > redCubes || green > greenCubes || blue > blueCubes)
+                return false;
+        }
+        return true;
+    }
+
+    private static int CountCubes(string set, string color)
+    {
+        var match = Regex.Match(set, $@"(\d+) {color}");
+        return match.Success ? int.Parse(match.Groups[1].Value) : 0;
+    }
+}
--- Days/2/Wmcgiff/Part2/Program.cs
+++ Days/2/Wmcgiff/Part2/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "./Input/Input.txt";
+        var gamesData = ReadGamesFromFile(filePath);
+
+        long totalPower = CalculateTotalPower(gamesData);
+        Console.WriteLine($"Total power of the sets: {totalPower}");
+    }
+
+    private static List<string> ReadGamesFromFile(string filePath)
+    {
+        var gamesData = new List<string>();
+        try
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    gamesData.Add(line);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error reading the file: {e.Message}");
+        }
+        return gamesData;
+    }
+
+    private static long CalculateTotalPower(List<string> gamesData)
+    {
+        long totalPower = 0;
+        foreach (var game in gamesData)
+        {
+            var (minRed, minGreen, minBlue) = CalculateMinimumCubes(game);
+            totalPower += (long)minRed * minGreen * minBlue;
+        }
+        return totalPower;
+    }
+
+    private static (int minRed, int minGreen, int minBlue) CalculateMinimumCubes(string gameData)
+    {
+        int minRed = 0, minGreen = 0, minBlue = 0;
+        var sets = gameData.Split(';');
+        foreach (var set in sets)
+        {
+            minRed = Math.Max(minRed, CountCubes(set, "red"));
+            minGreen = Math.Max(minGreen, CountCubes(set, "green"));
+            minBlue = Math.Max(minBlue, CountCubes(set, "blue"));
+        }
+        return (minRed, minGreen, minBlue);
+    }
+
+    private static int CountCubes(string set, string color)
+    {
+        var match = Regex.Match(set, $@"(\d+) {color}");
+        return match.Success ? int.Parse(match.Groups[1].Value) : 0;
+    }
+}
