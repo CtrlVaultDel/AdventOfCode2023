79f78647170b4e240bf8913797145423075e919e
Ryan DeVault <69012106+CtrlVaultDel@users.noreply.github.com>
2023-12-02 01:01:13 -0600

    Adding changes

---
+++ Days/2/ryan-devault/solution2.mjs
@@ -0,0 +1,11 @@
+// To Test:
+// 1. Click in file
+// 2. Ctrl + Alt + N
+import { getInput, getPerformance, logAnswer } from "../../../templates/ryan-devault/commonHelper.mjs"
+
+async function d2s2(){
+    const input = await getInput("./Days/1/ryan-devault/input.txt");
+}
+
+logAnswer(d2s2)
+// getPerformance(d2s2)
--- templates/ryan-devault/commonHelper.mjs
+++ templates/ryan-devault/commonHelper.mjs
@@ -9,3 +9,19 @@ export async function getInput(filePath) {
         return null;
     }
 }
+
+export async function getPerformance(testFunction, numTests = 10){
+    let performanceTimes = [];
+    for(let i = 0; i < numTests; i++){
+        const startTime = performance.now();
+        await testFunction()
+        performanceTimes.push(performance.now() - startTime);
+    }
+    const avgPerformance = performanceTimes.reduce((acc, cur) => acc += cur, 0)/performanceTimes.length
+    console.log(`${numTests} tests ran. Average performance: ${avgPerformance.toFixed(2)}ms`)
+}
+
+export async function logAnswer(func){
+    const answer = await func();
+    console.log("Answer:", answer);
+}
