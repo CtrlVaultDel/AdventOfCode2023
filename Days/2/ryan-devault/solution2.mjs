d384efa78b6c9c6a832e9dd8337dcf607ec57034
Ryan DeVault <69012106+CtrlVaultDel@users.noreply.github.com>
2023-12-02 01:26:33 -0600

    Finished Day2 Solution2

--- Days/2/ryan-devault/solution2.mjs
+++ Days/2/ryan-devault/solution2.mjs
@@ -4,8 +4,27 @@
 import { getInput, getPerformance, logAnswer } from "../../../templates/ryan-devault/commonHelper.mjs"
 
 async function d2s2(){
-    const input = await getInput("./Days/1/ryan-devault/input.txt");
+    const input = await getInput("./Days/2/ryan-devault/input.txt");
+    return input.reduce((acc, game) => {
+        const gameData = game.split(": ")
+        const rounds = gameData[1].replace("\r", "").split("; ")
+        const cubes = {
+            red: 0,
+            green: 0,
+            blue: 0
+        }
+        rounds.forEach(round => {
+            const picks = round.split(", ")
+            return picks.forEach(pick => {
+                const splitPick = pick.split(" ");
+                if(cubes[splitPick[1]] < splitPick[0]){
+                    cubes[splitPick[1]] = +splitPick[0]
+                }
+            }) 
+        })
+        return acc += (cubes.red * cubes.green * cubes.blue)
+    }, 0)
 }
 
 logAnswer(d2s2)
-// getPerformance(d2s2)
+getPerformance(d2s2)
